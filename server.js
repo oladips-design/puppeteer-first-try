11a5ab557a563ba8cf32a533db3d243423af9f8e
noobDEV0007 <oladeleoladipupo555@gmail.com>
2024-01-05 01:02:02 +0100

    fare

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const puppeteer = require("puppeteer");
-const { Browser } = puppeteer;
+const fs = require("fs");
+const { log } = require("console");
 
 const url = "https://books.toscrape.com";
 
-const main = async;
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const bookData = await page.evaluate((url) => {
+    const bookpods = Array.from(document.querySelectorAll(".product_pod"));
+    const data = bookpods.map((book) => ({
+      title: book.querySelector("h3 a").getAttribute("title"),
+      price: book.querySelector(".price_color").innerText,
+      imgSrc: url + book.querySelector("img").getAttribute("src"),
+      rating: book.querySelector(".star-rating").classList[1],
+    }));
+
+    return data;
+  }, url);
+  console.log(bookData);
+  await browser.close();
+
+  fs.writeFile("scraped.json", JSON.stringify(bookData), (err) => {
+    if (err) throw err;
+    console.log("done scraping");
+  });
+};
+
+main();
